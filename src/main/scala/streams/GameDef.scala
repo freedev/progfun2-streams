c75a2123a9c2e66c189322236a05c92c41142cd8
Vincenzo D'Amore <v.damore@gmail.com>
2016-08-04 17:53:41 +0200

    added GameDef methods

--- src/main/scala/streams/GameDef.scala
+++ src/main/scala/streams/GameDef.scala
@@ -84,7 +84,7 @@ trait GameDef {
    * This function returns the block at the start position of
    * the game.
    */
-  def startBlock: Block = ???
+  def startBlock: Block = Block(startPos, startPos)
 
 
   /**
@@ -135,22 +135,25 @@ trait GameDef {
      * Returns the list of blocks that can be obtained by moving
      * the current block, together with the corresponding move.
      */
-    def neighbors: List[(Block, Move)] = ???
+    def neighbors: List[(Block, Move)] = List((this.left, Left), (this.up, Up), (this.right, Right), (this.down, Down))
 
     /**
      * Returns the list of positions reachable from the current block
      * which are inside the terrain.
      */
-    def legalNeighbors: List[(Block, Move)] = ???
+    def legalNeighbors: List[(Block, Move)] = for {
+      nextMove <- neighbors
+      if nextMove._1.isLegal
+    } yield nextMove
 
     /**
      * Returns `true` if the block is standing.
      */
-    def isStanding: Boolean = ???
+    def isStanding: Boolean = b1.x == b2.x && b1.y == b2.y
 
     /**
      * Returns `true` if the block is entirely inside the terrain.
      */
-    def isLegal: Boolean = ???
+    def isLegal: Boolean = terrain(this.b1) && terrain(this.b2)
   }
 }
